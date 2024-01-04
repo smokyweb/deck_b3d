75a0cbdd1b68a03d11e622fc3b7bfcc7a582f544
Ken Bateman <kenbateman@bluestoneapps.com>
2024-01-04 15:45:01 -0500

    vertical slats in place.

--- src/three/railmaker.ts
+++ src/three/railmaker.ts
@@ -47,15 +47,51 @@ export class RailMaker {
     if (!topStock) {
       throw Error(`railtopStock '${spec.railTopStock}' doesn't exist`);
     }
-    group.add(this.newHoriz(spec, spec.railTopStock, 
-      spec.railTopInches - topStock.thickness/2));
+    const topRailCenterHeight = spec.railTopInches - topStock.thickness/2;
+    group.add(this.newHoriz(spec, spec.railTopStock, topRailCenterHeight));
     const bottomStock = LumberYard.lumberDimensions.get(spec.railBottomStock);
     if (!bottomStock) {
       throw Error(`railBottomStock '${spec.railBottomStock}' doesn't exist`);
     }
-    group.add(this.newHoriz(spec, spec.railBottomStock, 
-      spec.railBottomInches + bottomStock.thickness/2));
+    const bottomRailCenterHeight = spec.railBottomInches + bottomStock.thickness/2;
+    group.add(this.newHoriz(spec, spec.railBottomStock, bottomRailCenterHeight));
+
+    const postStock = LumberYard.lumberDimensions.get(spec.postStock);
+    if (!postStock) {
+      throw Error(`postStock '${spec.postStock}' doesn't exist`);
+    }
+    const basedist = spec.startBase.distanceTo(spec.endBase);
+    // rail is:   
+    //   1/2 post stock width
+    //   offset
+    //   nslats times:
+    //     1/2 slatInterval
+    //     slat centered here
+    //     1/2 slatInterval
+    //   offset
+    //   1/2 post stock width
+    // so postStockWidth + 2*offset + nslats*slatInterval = basedist
+    const nslats = Math.floor((basedist - postStock.width)/spec.slatIntervalInches);
+    const offset = (basedist - postStock.width - nslats*spec.slatIntervalInches)/2;
+    for (let i = 0; i < nslats; i++) {
+      const interpdist = postStock.width/2 + offset + i*spec.slatIntervalInches;
+      // normalized interpolation parameter, 0 to 1
+      const t = interpdist / basedist;
+      const slatloc = this.interp2(spec.startBase, spec.endBase, t);
+      const slatBase = Utils.deflatten(slatloc, bottomRailCenterHeight);
+      const slatTop = Utils.deflatten(slatloc, topRailCenterHeight);
+      const slat = this.lumberYard.makeLumberFromTo(spec.slatStock, slatBase, slatTop);
+      group.add(slat);
+    }
     return group;
+  }
+
+  
+  private interp1(a1: number, a2: number, x: number) {
+    return (a1*x) + (a2*(1-x));
+  }
+  private interp2(p1: THREE.Vector2, p2: THREE.Vector2, t: number) {
+    return new THREE.Vector2(this.interp1(p1.x, p2.x, t), this.interp1(p1.y, p2.y, t));
   }
 
   private newPost(spec: RailSpec, base2: THREE.Vector2, ): THREE.Mesh {
